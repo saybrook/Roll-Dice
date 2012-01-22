307724f33fb1f86b961f5ade1e3ab5f9e18252ed
Lucas Nicodemus <nicatron21@gmail.com>
2012-01-22 14:48:54 -0700

    Initial commit

---
+++ RollDice.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TShockAPI;
+using Terraria;
+
+namespace RollDice
+{
+	[APIVersion(1, 11)]
+	public class RollDice : TerrariaPlugin
+	{
+		public override Version Version
+		{
+			get { return new Version(1, 0); }
+		}
+
+		public override string Name
+		{
+			get { return "Dice Roll"; }
+		}
+
+		public override string Author
+		{
+			get { return "nicatronTg"; }
+		}
+
+		public override string Description
+		{
+			get { return "Roll the magical dice of Random"; }
+		}
+
+		public RollDice(Main game) : base(game)
+		{
+		}
+
+		public override void Initialize()
+		{
+			TShockAPI.Commands.ChatCommands.Add(new Command("roll", Roll, "roll"));
+		}
+
+		public void Roll(CommandArgs args)
+		{
+			Random r = new Random();
+			TShockAPI.TShock.Utils.Broadcast(args.Player.Name + " rolls a " + r.Next(0, 100), Color.Yellow);
+		}
+	}
+}
